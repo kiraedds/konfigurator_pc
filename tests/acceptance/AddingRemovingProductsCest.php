0c9547eff61ecfccf9c7362b15cbcc36139eab1c
Kamil Skorupa <skorupakamil@wp.pl>
2019-01-24 21:38:55 +0100

    Removing item test  working

--- tests/acceptance/AddingRemovingProductsCest.php
+++ tests/acceptance/AddingRemovingProductsCest.php
@@ -26,6 +26,7 @@ class AddingRemovingProductsCest
         $I->seeInCurrentUrl('/customer.php?value=plytyglowne');
         $I->see('dodaj do koszyka');
         $I->click('a[href = "customer.php?value=cartAction&action=addToCart&id=60"]');
+
         $I->see('twój koszyk');
         $I->click('a[href = "customer.php?value=viewCart"]');
 
@@ -39,8 +40,33 @@ class AddingRemovingProductsCest
 
         #adding working
 
+        $I->wantto('Remove Product from my basket');
+        $I->click('a[href = "customer.php"]');
+        $I->seeInTitle('KONFIGURATOR PC');
 
 
 
+        $I->see('płyty główne');
+        $I->click('płyty główne', 'a[href = "customer.php?value=plytyglowne"]');
+        $I->amOnPage("customer.php?value=plytyglowne");
+        $I->seeInCurrentUrl('/customer.php?value=plytyglowne');
+        $I->see('dodaj do koszyka');
+        $I->click('a[href = "customer.php?value=cartAction&action=addToCart&id=60"]');
+
+        $I->see('twój koszyk');
+        $I->click('a[href = "customer.php?value=viewCart"]');
+
+        $I->amOnPage("customer.php?value=viewCart");
+        $I->see('potwierdź zakupy');
+        $I->see('kontunuuj zakupy');
+
+        $I->click('a.btn.btn-danger');
+
+
+        $I->see('kontunuuj zakupy');
+        $I->dontsee('potwierdź zakupy');
+        $I->see('Twój koszyk jest pusty...');
+
+        #removing working
     }
 }
