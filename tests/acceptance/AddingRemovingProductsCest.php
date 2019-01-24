ac66e837e84efa3df73b6189024f611cff0cc5c2
Kamil Skorupa <skorupakamil@wp.pl>
2019-01-24 16:31:44 +0100

    Adding bucket test

--- tests/acceptance/AddingRemovingProductsCest.php
+++ tests/acceptance/AddingRemovingProductsCest.php
@@ -11,5 +11,12 @@ class AddingRemovingProductsCest
     {
 
         $I->wantto('Add Products to my basket');
+        $I->wantTo('see homepage');
+        $I->amOnPage("index.php");
+        $I->seeInTitle('KONFIGURATOR PC');
+        $I->fillField("login",'kowal');
+        $I->fillField("password",'kowal');
+        $I->click('ZALOGUJ');
+        $I->seeInCurrentUrl('/customer');
     }
 }
