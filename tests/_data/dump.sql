dc739063c938eb169316291b60e8006c24d5bd14
kiraed <dsmaluch@gmail.com>
2019-01-24 21:46:45 +0100

    basketW/Ologintest

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
 --
 -- Host: localhost    Database: test
 -- ------------------------------------------------------
--- Server version	5.7.24-0ubuntu0.18.04.1
+-- Server version	5.7.25-0ubuntu0.18.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -171,4 +171,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-22 16:29:32
+-- Dump completed on 2019-01-24 20:10:36
---
+++ tests/acceptance/TryBasket_NotLoggedInCest.php
@@ -0,0 +1,30 @@
+<?php 
+
+class TryBasket_NotLoggedInCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->wantto('add to basket without loging in');
+        $I->amonpage('index.php');
+        $I->seeInTitle('KONFIGURATOR PC');
+
+
+        $I->wantto('browse through some motherboards');
+        $I->see('płyty główne');
+        $I->click('a[href="index.php?value=plytyglowne"]');
+        $I->amOnPage("index.php?value=plytyglowne");
+        $I->seeInCurrentUrl('/index.php?value=plytyglowne');
+
+        $I->dontsee('dodaj do koszyka');
+        $I->see('Zaloguj sie, aby dodać do koszyka');
+
+        $I->comment("unble to add to cart while not logged in");
+
+
+    }
+}
